2870bb7f171ed157a54819467bb344890a91c854
Jared Gavigan <jagavigan80@gmail.com>
2021-09-07 22:56:13 -0400

    button clicked

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", function(event) {
+    console.log(event)
+  });
+  alert("button clicked")

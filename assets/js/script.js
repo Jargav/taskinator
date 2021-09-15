b18a0c6741eb0bd53c2bf4cd28bdbca7caaf654c
Jared Gavigan <jagavigan80@gmail.com>
2021-09-15 00:10:48 -0400

    appearance css

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,13 @@ var createTaskHandler = function() {
   }
 
 buttonEl.addEventListener("click", createTaskHandler); {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
-  alert("button clicked")
+  
+};
+ 
+  
+
+
+
 
   
 
